71bc6e9c3b2c8aa7c1ec84cda9d238939967bd8f
root <root@HOSTCOIN-2853-4220.com>
2026-04-17 18:28:17 +0530

    Commneted code removed

--- viewer-ui/src/App.jsx
+++ viewer-ui/src/App.jsx
@@ -1,34 +1,3 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-
-// function App() {
-//   const [score, setScore] = useState(null);
-
-//   const fetchScore = async () => {
-//     const res = await axios.get("http://localhost:5000/api/score");
-//     setScore(res.data);
-//   };
-
-//   useEffect(() => {
-//     fetchScore();
-//     const interval = setInterval(fetchScore, 2000); // polling for now
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   if (!score) return <h2>Loading...</h2>;
-
-//   return (
-//     <div style={{ textAlign: "center" }}>
-//       <h1>🏏 CricWorld</h1>
-//       <h2>{score.matchId}</h2>
-//       <h3>{score.score}</h3>
-//       <p>Over: {score.over}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { useEffect, useState } from "react";
 
 function App() {
