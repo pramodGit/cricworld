4ffe0070e56beccc3b225515660529ec5db9ace4
root <root@HOSTCOIN-2853-4220.com>
2026-04-25 18:24:05 +0530

    Multi-instance Websocket

--- websocket-server/index.js
+++ websocket-server/index.js
@@ -8,9 +8,49 @@ sub.subscribe("score-updates");
 
 wss.on("connection", (ws) => {
   console.log("🔌 Client connected");
+
+  ws.isAlive = true;
+
+  ws.on("pong", () => {
+    ws.isAlive = true;
+  });
+
+  ws.on("close", () => {
+    console.log("❌ Client disconnected");
+  });
 });
 
+// 💓 Heartbeat
+setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (!ws.isAlive) {
+      console.log("❌ Killing dead client");
+      return ws.terminate();
+    }
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
 sub.on("message", (channel, message) => {
+
+  console.log("📡 Redis received:", message);
+
+  let clientCount = 0;
+
+  wss.clients.forEach(client => {
+    clientCount++;
+
+    console.log("➡️ Client state:", client.readyState);
+
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+      console.log("✅ Sent to client");
+    }
+  });
+
+  console.log("👥 Total clients:", clientCount);
   const data = JSON.parse(message);
 
   wss.clients.forEach(client => {
